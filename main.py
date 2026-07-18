d8997536d2c33e400c893f8206264382b1034fe0
Laure Massemin <laure.massemin@gmail.com>
2026-07-18 22:10:55 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def extraire_gps_et_date(chemin_image) : 
+    pass
+
+def convertir_dms_en_decimal(degres, minutes, secondes, reference):
+    pass
+
+def scanner_dossier(dossier):
+    pass
+
+def generer_carte(points, chemin_sortie):
+    pass
+
+def main():
+    pass
+
+main()
